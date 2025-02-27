4b564a3ab16ead544f38cbf558742369096fb056
adishkt <adishadi01@gmail.com>
2025-02-27 15:00:25 +0530

    app jssx change
    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
-import Hom from "./components/Hom";
 import Mapp from "./components/Mapp";
 import Air from "./components/Air";
-import Team from "./components/Team";
+
 
 
 
@@ -13,10 +12,9 @@ function App() {
     <>
       <Nav/>
       <Routes>
-        <Route path="/" element={<Hom/>} />
-        <Route path="/map1" element={<Mapp/>} />
+        <Route path="/" element={<Mapp/>} />
         <Route path="/map2" element={<Air/>} />
-        <Route path="/team" element={<Team/>} />
+        
 
 
 
