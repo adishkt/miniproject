c831105c132a15bc42d10fcad9de361262688d41
adishkt <adishadi01@gmail.com>
2025-03-30 00:22:42 +0530

    mapupdate

--- src/components/Mapp.jsx
+++ src/components/Mapp.jsx
@@ -13,7 +13,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { Typography, ButtonGroup, Button } from "@mui/material";
-import "./air.css"
+import "./air.css";
 
 const geoUrl = "/features.json";
 
@@ -25,29 +25,32 @@ const MapChart = () => {
   const [data, setData] = useState([]);
   const [tooltipContent, setTooltipContent] = useState("");
   const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
-  const [selectedDate, setSelectedDate] = useState(null);
   const [year, setYear] = useState(null);
   const [month, setMonth] = useState(null);
   const [zoomLevel, setZoomLevel] = useState(1);
 
   useEffect(() => {
-    csv(`/Temperature Predictions from Model 1.1.csv`).then((data) => {
-      console.log("CSV Data:", data);
-      setData(data);
+    csv(`/Temperature Predictions.csv`).then((csvData) => {
+      const formattedData = csvData.map(d => {
+        return {
+          ...d,
+          year: parseInt(d.year, 10),
+          month: parseInt(d.month, 10),
+          predicted_temperature: parseFloat(d.predicted_temperature)
+        };
+      });
+      setData(formattedData);
     });
   }, []);
 
-  useEffect(() => {
-    fetch(geoUrl)
-      .then((response) => response.json())
-      .then((geoData) => console.log("GeoJSON Data:", geoData));
-  }, []);
+  const filteredData = data.filter((d) => {
+    return (!year || d.year === year) && (!month || d.month === month);
+  });
 
   const handleDateChange = (date) => {
     if (date) {
-      setSelectedDate(date);
       setYear(date.year());
-      setMonth(date.month() + 1);
+      setMonth(date.month() + 1); // Using date.month() returns 0-indexed month, so adding 1
     }
   };
 
@@ -58,13 +61,7 @@ const MapChart = () => {
     setTooltipPosition({ x: event.pageX, y: event.pageY });
   };
 
-  const handleMouseMove = (event) => {
-    setTooltipPosition({ x: event.pageX, y: event.pageY });
-  };
-
-  const handleMouseLeave = () => {
-    setTooltipContent("");
-  };
+  const handleMouseLeave = () => setTooltipContent("");
 
   const handleZoomIn = () => setZoomLevel((prev) => Math.min(prev * 1.5, 10));
   const handleZoomOut = () => setZoomLevel((prev) => Math.max(prev / 1.5, 1));
@@ -72,7 +69,7 @@ const MapChart = () => {
   return (
     <div style={{ position: "relative", width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <Typography variant="h3" component="div" sx={{ color: "white", fontFamily: "fantasy", paddingTop: "20px" }}>
-       Surface Temperature
+        Surface temperature
       </Typography>
       <br />
       <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -82,7 +79,7 @@ const MapChart = () => {
           onChange={handleDateChange}
           sx={{ width: "420px", backgroundColor: "yellow" }}
         />
-        <h1>Selected Year: {year}</h1>
+        <h1>Selected Year: {year}, Month: {month}</h1>
       </LocalizationProvider>
 
       <ButtonGroup style={{ margin: "10px" }}>
@@ -90,48 +87,36 @@ const MapChart = () => {
         <Button onClick={handleZoomOut}>Zoom Out</Button>
       </ButtonGroup>
 
-      <div style={{ display: "flex", position: "relative", width: "100%", justifyContent: "center" }}>
-        <div className="legend">
-          <div className="legend-gradient"></div>
-          <div className="legend-labels">
-            {[-15, 0, 15, 30, 45, 60, 75, 95].map((temp, index) => (
-              <span key={index} className="legend-label">{temp}°C</span>
-            ))}
-          </div>
-        </div>
-
-        <ComposableMap projectionConfig={{ rotate: [-10, 0, 0], scale: 147 }}>
-          <ZoomableGroup zoom={zoomLevel}>
-            <Sphere stroke="#00000" strokeWidth={0.5} />
-            <Graticule stroke="#00000" strokeWidth={0.5} />
-            {data.length > 0 && (
-              <Geographies geography={geoUrl}>
-                {({ geographies }) =>
-                  geographies.map((geo) => {
-                    const d = data.find((s) => s.country === geo.properties.name);
-                    const countryData = d ? parseFloat(d.predicted_temperature) : undefined;
-                    return (
-                      <Geography
-                        key={geo.rsmKey}
-                        geography={geo}
-                        fill={countryData !== undefined ? colorScale(countryData) : "#F5F4Fb"}
-                        onMouseEnter={(event) => handleMouseEnter(event, geo, d)}
-                        onMouseMove={handleMouseMove}
-                        onMouseLeave={handleMouseLeave}
-                        style={{
-                          default: { outline: "none" },
-                          hover: { outline: "none", fill: "#FF5722" },
-                          pressed: { outline: "none" }
-                        }}
-                      />
-                    );
-                  })
-                }
-              </Geographies>
-            )}
-          </ZoomableGroup>
-        </ComposableMap>
-      </div>
+      <ComposableMap projectionConfig={{ rotate: [-10, 0, 0], scale: 147 }}>
+        <ZoomableGroup zoom={zoomLevel}>
+          <Sphere stroke="#00000" strokeWidth={0.5} />
+          <Graticule stroke="#00000" strokeWidth={0.5} />
+          {filteredData.length > 0 && (
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => {
+                  const d = filteredData.find((s) => s.country === geo.properties.name);
+                  const countryData = d ? parseFloat(d.predicted_temperature) : undefined;
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      fill={countryData !== undefined ? colorScale(countryData) : "#F5F4Fb"}
+                      onMouseEnter={(event) => handleMouseEnter(event, geo, d)}
+                      onMouseLeave={handleMouseLeave}
+                      style={{
+                        default: { outline: "none" },
+                        hover: { outline: "none", fill: "#FF5722" },
+                        pressed: { outline: "none" }
+                      }}
+                    />
+                  );
+                })
+              }
+            </Geographies>
+          )}
+        </ZoomableGroup>
+      </ComposableMap>
 
       {tooltipContent && (
         <div
@@ -140,11 +125,10 @@ const MapChart = () => {
             top: tooltipPosition.y + 10,
             left: tooltipPosition.x + 10,
             backgroundColor: "black",
+            color: "white",
             padding: "10px",
             borderRadius: "5px",
-            boxShadow: "0 4px 6px rgba(37, 36, 36, 0.1)",
-            pointerEvents: "none",
-            fontSize: "16px"
+            pointerEvents: "none"
           }}
         >
           {tooltipContent}
