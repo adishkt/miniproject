597ec91285a15b85f7bc49e7ed24bc577c97b3ac
GeraldoRiberia <91908767+GeraldoRiberia@users.noreply.github.com>
2025-04-03 12:03:12 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Mapp from "./components/Mapp";
-import Air from "./components/Air";
+import GHG from "./components/GHG";
 import { Grid2 } from "@mui/material";
 import './App.css';
 
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         
         <Route path="/" element={<Mapp/>} />
-        <Route path="/map2" element={<Air/>} />
+        <Route path="/map2" element={<GHG/>} />
        
       </Routes>
     </>
